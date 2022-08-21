15e4bd56d915e2702f5bc77b448add81c1ce6243
Dmitriy <105502040+KnyazevDmitriy@users.noreply.github.com>
2022-08-21 13:49:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ function calc(a, b) {
     function fn(action) {
 
         if (typeof a === 'number' && typeof b === 'number') { 
+
             switch (action) {
                 case 'add':
                     return a + b
                 case 'subtract':
-                    return a - b || null
+                    return a - b
                 case 'multiply':
-                    return a * b || null
+                    return a * b
                 case 'divide':
-                    return a / b || null
+                    return a / b
                 default:
                     return null
             }
